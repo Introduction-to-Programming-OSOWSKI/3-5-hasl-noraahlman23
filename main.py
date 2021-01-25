33a9c0cb0bee1431d436d922913598e93c3be2a1
noraahlman23 <819446@mystma.org>
2021-01-25 15:57:15 +0000

    hasL

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#WRITE YOUR CODE IN THIS FILE
+def hasL(w):
+    
+    for i in range(0, len(w)):
+        if w[i] == "l":
+            return True
+    
+    return False
+
+print (hasL("alabama"))
